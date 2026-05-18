12e0c7a1346ae164b6de1c4656143ece6e9022a4
Lucas <109859872+iamdevlucas123@users.noreply.github.com>
2026-05-18 15:09:30 -0300

    Update user_route.ts
    Created all the user routes

---
+++ api/src/routes/user_route.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express'
+import { userController } from '../controllers/user_controller'
+
+const userRouter = Router()
+
+userRouter.post('/', userController.create);
+userRouter.get('/', userController.findAll);
+userRouter.get('/:id', userController.findById);
+userRouter.put('/:id', userController.update);
+userRouter.delete('/:id', userController.delete);
+
+export { userRouter }
