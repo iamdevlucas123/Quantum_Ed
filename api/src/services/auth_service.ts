fd5ce244ef9db8286084e891f3518bc7b889c204
Lucas <109859872+iamdevlucas123@users.noreply.github.com>
2026-05-20 14:55:13 -0300

    Update auth_service

--- api/src/services/auth_service.ts
+++ api/src/services/auth_service.ts
@@ -4,7 +4,7 @@ import { User, UserRole } from '@prisma/client'
 import { env } from '../config/env'
 import { prisma } from '../config/prisma'
 
-type SignUp = {
+type SignUpData = {
   name?: string;
   email: string;
   password: string;
@@ -37,7 +37,7 @@ const sanitizeUser = (user: User): AuthUser => {
   return safeUser
 }
 
-//Its define the sign options
+//Its sign the jwt accessToken
 const signAccessToken = (user:User): string => {
   const payload: JwtPayload = {
     sub: user.id,
@@ -49,6 +49,57 @@ const signAccessToken = (user:User): string => {
     expiresIn: (env.JWT_EXPIRES_IN || '1d') as jwt.SignOptions['expiresIn'],
   }
 
-  return jwt.sign(payload, env.JWT_EXPIRES_IN, options)
+  return jwt.sign(payload, env.JWT_SECRET, options)
 }
 
+export const authService = {
+  async signUp(data: SignUpData): Promise<AuthResponse> {
+    const existingUser = prisma.user.findUnique({
+      where: {email: data.email}
+    })
+
+    if (!existingUser) {
+      throw new Error('email already exist')
+    }
+
+    const passwordHash = await bcrypt.hash(data.password, SALT_ROUNDS)
+
+    const user = await prisma.user.create({
+      data: {
+        name: data.name,
+        email: data.email,
+        passwordHash,
+        role: data.role,
+      }
+    })
+
+    return {
+      user: sanitizeUser(user),
+      accessToken: signAccessToken(user)
+    }
+  },
+
+  async signIn(data: SignInData): Promise<AuthResponse> {
+    const user = await prisma.user.findUnique({
+      where: { email: data.email },
+    });
+    if (!user) {
+      throw new Error('Invalid credentials');
+    }
+    const isPasswordValid = await bcrypt.compare(data.password, user.passwordHash);
+    if (!isPasswordValid) {
+      throw new Error('Invalid credentials');
+    }
+    return {
+      user: sanitizeUser(user),
+      accessToken: signAccessToken(user),
+    };
+  },
+
+  verifyAccessToken(token: string): JwtPayload {
+    if (!env.JWT_SECRET) {
+      throw new Error('JWT_SECRET is not configured');
+    }
+    return jwt.verify(token, env.JWT_SECRET) as JwtPayload;
+  },
+}
